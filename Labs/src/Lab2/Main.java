4995b82abfacf0061bf3f20f612fae5d6f0dccaa
Marcus <marcus3111@gmail.com>
2016-02-10 13:54:46 +0100

    fixed sumDistances

--- Labs/src/Lab2/Main.java
+++ Labs/src/Lab2/Main.java
@@ -1,11 +1,17 @@
 package Lab2;
 
+import java.util.ArrayList;
+
 import org.jacop.constraints.Constraint;
 import org.jacop.constraints.Element;
+import org.jacop.constraints.IfThen;
+import org.jacop.constraints.Or;
+import org.jacop.constraints.PrimitiveConstraint;
 import org.jacop.constraints.Subcircuit;
 import org.jacop.constraints.Sum;
 import org.jacop.constraints.XeqY;
 import org.jacop.constraints.XneqC;
+import org.jacop.constraints.XneqY;
 import org.jacop.core.IntVar;
 import org.jacop.core.Store;
 import org.jacop.search.DepthFirstSearch;
@@ -13,7 +19,7 @@ import org.jacop.search.IndomainMin;
 import org.jacop.search.PrintOutListener;
 import org.jacop.search.Search;
 import org.jacop.search.SelectChoicePoint;
-import org.jacop.search.SimpleSelect;
+import org.jacop.search.SimpleMatrixSelect;
 
 public class Main {
 
@@ -64,31 +70,56 @@ public class Main {
 			store.impose(new Subcircuit(x[i]));
 			store.impose(new XneqC(x[i][dest[i]], dest[i] + 1));
 		}
-
-		IntVar[] d = new IntVar[graph_size];
-		for (int i = 0; i < graph_size; i++) {
-			d[i] = new IntVar(store, "dist" + i, 0, 1000);
-			IntVar v = new IntVar(store, 0, 100);
-			store.impose(new Element(x[0][i], distances[i], v, 0));
-			store.impose(new XeqY(d[i], v));
+		ArrayList<PrimitiveConstraint> c = new ArrayList<PrimitiveConstraint>();
+		IntVar[] vs = new IntVar[x[0].length];
+		for (int i = 0; i < x[0].length; i++) {
+			vs[i] = new IntVar(store, 0, 100);
+			store.impose(new Element(x[0][i], x[1], vs[i]));
+			c.add(new IfThen(new XneqC(vs[i], 0), new XneqY(x[0][i], vs[i])));
 		}
+		store.impose(new Or(c));
+		// store.impose(new XneqC(x[1][5],6));
 
-		IntVar distance = new IntVar(store, 0, 1000);
-		Constraint distanceCost = new Sum(d, distance);
-		store.impose(distanceCost);
+		IntVar[][] d = new IntVar[x.length][graph_size];
+		for (int j = 0; j < d.length; j++)
+			for (int i = 0; i < graph_size; i++) {
+				d[j][i] = new IntVar(store, "dist" + i, 0, 1000);
+				IntVar v = new IntVar(store, 0, 100);
+				store.impose(new Element(x[j][i], distances[i], v));
+				store.impose(new XeqY(d[j][i], v));
+			}
+
+		IntVar distance[] = new IntVar[d.length];
+		for (int i = 0; i < distance.length; i++) {
+			distance[i] = new IntVar(store, 0, 1000);
+			Constraint distanceCost = new Sum(d[i], distance[i]);
+			store.impose(distanceCost);
+		}
+		IntVar distance2 = new IntVar(store, 0, 1000);
+		store.impose(new Sum(distance, distance2));
 		Search<IntVar> search = new DepthFirstSearch<IntVar>();
-		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(d, null, new IndomainMin<IntVar>());
+		SelectChoicePoint<IntVar> select = new SimpleMatrixSelect<IntVar>(x, new IndomainMin<IntVar>());
+		// SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(distance,
+		// null, new IndomainMin<IntVar>());
 
 		search.setSolutionListener(new PrintOutListener<IntVar>());
-		boolean Result = search.labeling(store, select, distance);
+		boolean Result = search.labeling(store, select, distance2);
 		if (Result) {
 			System.out.println(store);
 			System.out.println("\n***Yes");
-			System.out.println("Price: " + distance.value());
+			System.out.println("Price: " + distance2.value());
 			System.out.println("\nFrom \tTo");
 			for (int i = 0; i < graph_size; i++)
 				System.out.println(i + "\t" + (x[0][i].value() - 1));
+			System.out.println();
+			for (int i = 0; i < graph_size; i++)
+				System.out.println(i + "\t" + (x[1][i].value() - 1));
 		}
+		// for(int i=0;i<distances.length;i++){
+		// for(int j=0;j<distances[0].length;j++)
+		// System.out.print(distances[i][j]+ "\t");
+		// System.out.println();
+		// }
 	}
 }
 
