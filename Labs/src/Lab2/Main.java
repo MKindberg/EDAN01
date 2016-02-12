f50d03c1a936f1b792829a8b01c728cfd23157e6
Marcus Kindberg <tpi12mki@gamma-9.student.lth.se>
2016-02-12 09:24:56 +0100

    Lab 2 done

--- Labs/src/Lab2/Main.java
+++ Labs/src/Lab2/Main.java
@@ -1,9 +1,15 @@
-package Lab2;
+package logistics;
+
+import java.util.ArrayList;
 
 import org.jacop.constraints.Constraint;
 import org.jacop.constraints.Element;
+import org.jacop.constraints.IfThenElse;
+import org.jacop.constraints.Or;
+import org.jacop.constraints.PrimitiveConstraint;
 import org.jacop.constraints.Subcircuit;
 import org.jacop.constraints.Sum;
+import org.jacop.constraints.XeqC;
 import org.jacop.constraints.XeqY;
 import org.jacop.constraints.XneqC;
 import org.jacop.core.IntVar;
@@ -13,14 +19,14 @@ import org.jacop.search.IndomainMin;
 import org.jacop.search.PrintOutListener;
 import org.jacop.search.Search;
 import org.jacop.search.SelectChoicePoint;
-import org.jacop.search.SimpleSelect;
+import org.jacop.search.SimpleMatrixSelect;
 
-public class Main {
+public class Main3 {
 
 	public static void main(String[] args) {
 		long T1, T2, T;
 		T1 = System.currentTimeMillis();
-		logistics(new Data2());
+		logistics(new Data3());
 		T2 = System.currentTimeMillis();
 		T = T2 - T1;
 		System.out.println("\n\t***	Execution time = " + T + " ms");
@@ -39,108 +45,110 @@ public class Main {
 		int dest[] = data.dest;
 
 		int[][] distances = new int[graph_size][graph_size];
-		for (int i = 1; i < graph_size; i++)
-			for (int j = 1; j < graph_size; j++) {
+		for (int i = 0; i < graph_size; i++)
+			for (int j = 0; j < graph_size; j++) {
 				distances[i][j] = 100;
 				if (i == j)
 					distances[i][j] = 0;
-
 			}
 
 		for (int i = 0; i < from.length; i++) {
 			distances[from[i]][to[i]] = cost[i];
 			distances[to[i]][from[i]] = cost[i];
 		}
+		for (int i = 0; i < n_dests; i++)
+			distances[dest[i]][start] = 0;
 
+		int[] cost2 = new int[graph_size * graph_size];
+		for (int i = 0; i < graph_size; i++)
+			for (int j = 0; j < graph_size; j++)
+				cost2[j + i * graph_size] = distances[i][j];
+				
+		
 		IntVar[][] x = new IntVar[n_dests][graph_size];
 		for (int i = 0; i < n_dests; i++)
 			for (int j = 0; j < graph_size; j++)
 				x[i][j] = new IntVar(store, "x" + i + " " + j, 0, graph_size);
 
-		store.impose(new Subcircuit(x[0]));
-		store.impose(new XneqC(x[0][start], start + 1));
-		store.impose(new XneqC(x[0][dest[0]], dest[0] + 1));
-		for (int i = 1; i < x.length; i++) {
+		/*alla måste starta i start och sluta i sin egen dest*/
+		for (int i = 0; i < x.length; i++) {
 			store.impose(new Subcircuit(x[i]));
+			store.impose(new XneqC(x[i][start], start + 1));
 			store.impose(new XneqC(x[i][dest[i]], dest[i] + 1));
 		}
 
-		IntVar[] d = new IntVar[graph_size];
+		IntVar[][] d = new IntVar[graph_size][n_dests];
+		for (int i = 0; i < graph_size; i++)
+			for (int j = 0; j < n_dests; j++) {
+				d[i][j] = new IntVar(store, "dist" + j, 0, 1000);
+				IntVar v = new IntVar(store, 0, 100);
+				store.impose(new Element(x[j][i], distances[i], v));
+				//store.impose(new XeqY(d[i][j], v));
+				
+				ArrayList<PrimitiveConstraint> s = new ArrayList<PrimitiveConstraint>();
+				for(int k=j+1;k<n_dests;k++){
+						s.add(new XeqY(x[j][i], x[k][i]));
+				}
+				store.impose(new IfThenElse(new Or(s), new XeqC(d[i][j], 0), new XeqY(d[i][j], v)));
+			}
+
+//		for (int i = 0; i < graph_size; i++)
+//			for (int j = 0; j < n_dests; j++) {
+//				IntVar v = new IntVar(store, 0, 100);
+//				store.impose(new Element(d[i][j], distances[i], v));
+//			}
+			
+		
+		IntVar[] d2 = new IntVar[graph_size];
 		for (int i = 0; i < graph_size; i++) {
-			d[i] = new IntVar(store, "dist" + i, 0, 1000);
-			IntVar v = new IntVar(store, 0, 100);
-			store.impose(new Element(x[0][i], distances[i], v, 0));
-			store.impose(new XeqY(d[i], v));
+			d2[i] = new IntVar(store, 0, 100);
+			IntVar sum = new IntVar(store, 0, 100);
+
+			store.impose(new Sum(d[i], sum));
 		}
 
-		IntVar distance = new IntVar(store, 0, 1000);
-		Constraint distanceCost = new Sum(d, distance);
-		store.impose(distanceCost);
+		IntVar distance[] = new IntVar[d.length];
+		for (int i = 0; i < distance.length; i++)
+
+		{
+			distance[i] = new IntVar(store, 0, 1000);
+			Constraint distanceCost = new Sum(d[i], distance[i]);
+			store.impose(distanceCost);
+		}
+
+		IntVar distance2 = new IntVar(store, 0, 1000);
+		store.impose(new Sum(distance, distance2));
 		Search<IntVar> search = new DepthFirstSearch<IntVar>();
-		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(d, null, new IndomainMin<IntVar>());
+		SelectChoicePoint<IntVar> select = new SimpleMatrixSelect<IntVar>(x, new IndomainMin<IntVar>());
+		// SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(distance,
+		// null, new IndomainMin<IntVar>());
 
 		search.setSolutionListener(new PrintOutListener<IntVar>());
-		boolean Result = search.labeling(store, select, distance);
-		if (Result) {
+		boolean Result = search.labeling(store, select, distance2);
+		if (Result)
+
+		{
 			System.out.println(store);
 			System.out.println("\n***Yes");
-			System.out.println("Price: " + distance.value());
+			System.out.println("Price: " + distance2.value());
 			System.out.println("\nFrom \tTo");
+			for(int j=0;j<n_dests;j++){
 			for (int i = 0; i < graph_size; i++)
-				System.out.println(i + "\t" + (x[0][i].value() - 1));
+				System.out.println(i + "\t" + (x[j][i].value() - 1));
+			System.out.println();
+			}
 		}
-	}
-}
 
-class Data {
-	int graph_size;
-	int start;
-	int n_dests;
-	int[] dest;
-	int n_edges;
-	int[] from;
-	int[] to;
-	int[] cost;
-}
+		System.out.println("\n");
+		for (
 
-class Data1 extends Data {
+		int i = 0; i < d.length; i++)
 
-	public Data1() {
-		graph_size = 6;
-		start = 1;
-		n_dests = 1;
-		dest = new int[] { 6 };
-		n_edges = 7;
-		from = new int[] { 1, 1, 2, 2, 3, 4, 4 };
-		to = new int[] { 2, 3, 3, 4, 5, 5, 6 };
-		cost = new int[] { 4, 2, 5, 10, 3, 4, 11 };
-	}
-}
+		{
+			for (int j = 0; j < d[0].length; j++)
+				System.out.print(d[i][j].value() + "\t");
+			System.out.println();
+		}
 
-class Data2 extends Data {
-
-	public Data2() {
-		graph_size = 6;
-		start = 1;
-		n_dests = 2;
-		dest = new int[] { 5, 6 };
-		n_edges = 7;
-		from = new int[] { 1, 1, 2, 2, 3, 4, 4 };
-		to = new int[] { 2, 3, 3, 4, 5, 5, 6 };
-		cost = new int[] { 4, 2, 5, 10, 3, 4, 11 };
-	}
-}
-
-class Data3 extends Data {
-
-	public Data3() {
-		graph_size = 6;
-		start = 1;
-		n_dests = 2;
-		dest = new int[] { 5, 6 };
-		n_edges = 9;
-		from = new int[] { 1, 1, 1, 2, 2, 3, 3, 3, 4 };
-		to = new int[] { 2, 3, 4, 3, 5, 4, 5, 6, 6 };
-		cost = new int[] { 6, 1, 5, 5, 3, 5, 6, 4, 2 };
 	}
 }
